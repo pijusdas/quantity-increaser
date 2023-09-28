40fd2ee93ffdd82a835491a9be0e07f1b1737662
pijusdas <pijusdas.dev@gmail.com>
2023-09-28 20:02:14 +0600

    done

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 document.getElementById('plus').addEventListener('click', function() {
     const num1 = parseFloat(document.getElementById('num').innerText);
     const result = num1 + 1;
-    console.log(result)
     document.getElementById('num').textContent = `${result}`;
 });
 document.getElementById('minus').addEventListener('click', function() {
     const num1 = parseFloat(document.getElementById('num').innerText);
     const result = num1 - 1;
-    console.log(result)
     document.getElementById('num').textContent = `${result}`;
 });
